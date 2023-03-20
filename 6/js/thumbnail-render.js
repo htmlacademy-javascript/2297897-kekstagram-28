539a3ab42dbe11db99ac587abf7e4f15cc060282
Keks <keks@htmlacademy.ru>
2023-03-20 11:53:12 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/thumbnail-render.js
@@ -0,0 +1,20 @@
+import {photoInfoList} from './data.js';
+
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+const pictureListFragment = document.createDocumentFragment();
+
+photoInfoList.forEach(({url, description, likes, comments, id}) =>{
+  const newRender = pictureTemplate.cloneNode(true);
+
+  newRender.querySelector('.picture__img').src = url;
+  newRender.querySelector('.picture__img').alt = description;
+  newRender.querySelector('.picture__likes').textContent = likes;
+  newRender.querySelector('.picture__comments').textContent = comments.length;
+  newRender.dataset.thumbnailId = id;
+  pictureListFragment.append(newRender);
+});
+
+picturesContainer.append(pictureListFragment);
